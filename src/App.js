cf5f17bec63493b7199cebb0c3d51d2d65e9f497
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-31 21:13:45 +1000

    created header and home component files - beginning build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Header */}
+      {/* <Header /> */}
+      {/* Home */}
+      <Home />
+      {/* Section */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Tesla</h1>
+    </div>
+  );
+};
+
+export default Header;
