c1ba4ce96274aa205d579138295f3553253e555c
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-06-01 19:02:33 +1000

    added material-ui packages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,57 @@
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Tesla</h1>
-    </div>
+    <Container>
+      <a href="#">
+        <img src="/images/logo.svg" alt="tesla logo" />
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <Menu />
+      </RightMenu>
+    </Container>
   );
 };
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+const Menu = styled.div`
+  display: flex;
+  alignt-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
+
+const RightMenu = styled.div`
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin: 10px;
+  }
+`;
