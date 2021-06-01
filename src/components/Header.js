c16139c3f32ed2a97e748997d9aebf9259b1bfdd
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-06-01 20:33:09 +1000

    added react-reveal package - updated styling in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <Menu />
+        <CustomMenu />
       </RightMenu>
     </Container>
   );
@@ -29,11 +29,13 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
 `;
+
 const Menu = styled.div`
   display: flex;
   alignt-items: center;
@@ -46,12 +48,22 @@ const Menu = styled.div`
     padding: 0 10px;
     flex-wrap: nowrap;
   }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
   a {
     font-weight: 600;
     text-transform: uppercase;
     margin: 10px;
   }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
 `;
